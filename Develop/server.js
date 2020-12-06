46eada4212e1d368f56c4e69042e3bb41b054d1b
NPitroff <40879599+NPitroff@users.noreply.github.com>
2020-12-06 15:56:40 -0800

    ADDED PATHS AND COMMANDS TO THE SERVER.JS

---
+++ Develop/server.js
@@ -0,0 +1,17 @@
+//================LOAD THE CARIABLE FOR THE EXPRESS MODULE====================//
+var express = require("express")
+//================DECLARE THE EXPRESS NODE FUNCTION===========================//
+var app = express();
+//================PORT THE SERVER WILL NEED TO LISTEN TO======================//
+var PORT = process.env.PORT || 8080;
+//================DECLARE PATHS FOR THE EXPRESS FUNCTION======================//
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.static('public'));
+//=================DECLARE ROUTES FOR FILE ACCESS=============================//
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+//=================COMMAND TO START THE SERVER================================//
+app.listen(PORT, () =>{
+    console.log("SERVER IS LISTENING AT PORT 8080.")
+});
