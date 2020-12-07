21c74953f0b4b639d012cec934d8188922161a19
NPitroff <40879599+NPitroff@users.noreply.github.com>
2020-12-06 20:05:04 -0800

    Fixed variables

--- Develop/server.js
+++ Develop/server.js
@@ -1,12 +1,12 @@
 //================LOAD THE CARIABLE FOR THE EXPRESS MODULE====================//
-var express = require("express")
+const express = require("express")
 //=================DECLARE ROUTES FOR FILE ACCESS=============================//
-var apiRoutes = require("./routes/apiRoutes")(app);
-var htmlRoutes = require("./routes/htmlRoutes")(app);
+ require("./routes/apiRoutes")(app);
+ require("./routes/htmlRoutes")(app);
 //================DECLARE THE EXPRESS NODE FUNCTION===========================//
-var app = express();
+const app = express();
 //================PORT THE SERVER WILL NEED TO LISTEN TO======================//
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 //================DECLARE PATHS FOR THE EXPRESS FUNCTION======================//
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
